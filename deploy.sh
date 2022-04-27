e6be43b9ca42d40d55c04ea04eaac3e7ac61e52c
zCoder <cycs.ingenieria@gmail.com>
2022-04-27 17:34:23 -0400

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:<cycscarlos>/<cycscarlos>.github.io.git main
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<cycscarlos>/<repo>.git main:gh-pages
+
+cd -
+
